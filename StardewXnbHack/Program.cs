aeaf8892832e5967acc3b132a34b5d4e1befb656
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-06-16 20:10:31 -0400

    add basic structure

---
+++ StardewXnbHack/Program.cs
@@ -0,0 +1,14 @@
+namespace StardewXnbHack
+{
+    /// <summary>The console app entry point.</summary>
+    internal class Program
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The console app entry method.</summary>
+        static void Main()
+        {
+        }
+    }
+}
