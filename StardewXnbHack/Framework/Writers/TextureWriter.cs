6648be2380ad481fbfb200185463f476d5b1ab49
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-08-01 21:40:42 -0400

    reverse premultiplication applied to XNA image assets

--- StardewXnbHack/Framework/Writers/TextureWriter.cs
+++ StardewXnbHack/Framework/Writers/TextureWriter.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI.Toolkit.Utilities;
 
@@ -29,11 +30,40 @@ namespace StardewXnbHack.Framework.Writers
         {
             Texture2D texture = (Texture2D)asset;
 
+            this.UnpremultiplyTransparency(texture);
             using (Stream stream = File.Create($"{toPathWithoutExtension}.png"))
                 texture.SaveAsPng(stream, texture.Width, texture.Height);
 
             error = null;
             return true;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Reverse premultiplication applied to an image asset by the XNA content pipeline.</summary>
+        /// <param name="texture">The texture to adjust.</param>
+        private void UnpremultiplyTransparency(Texture2D texture)
+        {
+            Color[] data = new Color[texture.Width * texture.Height];
+            texture.GetData(data);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Color pixel = data[i];
+                if (pixel.A == 0)
+                    continue;
+
+                data[i] = new Color(
+                    r: (byte)((pixel.R * 255) / pixel.A),
+                    g: (byte)((pixel.G * 255) / pixel.A),
+                    b: (byte)((pixel.B * 255) / pixel.A),
+                    a: pixel.A
+                );
+            }
+
+            texture.SetData(data);
         }
     }
 }
